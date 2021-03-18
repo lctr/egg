d14b40cf64339f5327d6ddb411f92e0fcc2bb260
Lictor Guzman <lctrgzmn@gmail.com>
2021-03-18 02:19:50 -0500

    separating entry point from static files

---
+++ main.js
@@ -0,0 +1,78 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 500,
+    height: 350,
+    darkTheme: true,
+    backgroundColor: "#80111111",
+    // titleBarStyle: "hiddenInset",
+    visualEffectState: "followWindow",
+    vibrancy: "sheet",
+    webPreferences: {
+      nodeIntegration: true,
+      textAreasAreResizable: false,
+      defaultFontFamily: {
+        standard: "Helvetica"
+      },
+      spellcheck: false,
+
+    }
+  });
+  win.loadFile('src/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  /* if (process.platform !== 'darwin') */ app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      { label: 'New' },
+      { label: 'Open' },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click () { app.quit(); }
+      }
+    ]
+  }
+];
+
+
+// add empty object to menu for mac
+if (process.platform == 'darwin') { mainMenuTemplate.unshift({}); }
+
+// add dev tools if not in prod
+if (process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click (item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  });
+}
+
+
+
+
