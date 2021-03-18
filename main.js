f18db59e47f25f1bf2e1eef53977246e8f729b93
Lictor Guzman <lctrgzmn@gmail.com>
2021-03-18 07:56:27 -0500

    disabled exit on close

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function createWindow () {
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  /* if (process.platform !== 'darwin') */ app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
